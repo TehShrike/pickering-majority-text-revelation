b4e5e8ffa8355f48798886682db40b2506a59d73
Josh Duff <me@JoshDuff.com>
2017-02-25 14:11:34 -0600

    Chunking up verse 17 + noting section numbers of verse chunks

--- convertor/3-convert-basic-json-to-final-form.js
+++ convertor/3-convert-basic-json-to-final-form.js
@@ -1,96 +1,14 @@
 const fs = require('fs')
 
-const input = require('../very-basic-parsed.json')
 const parseVerse = require('./parse-verse-number')
 
-const relevant = input.filter(o => o.type !== 'UNKNOWN' && !(o.text && o.text.toLowerCase() === 'revelation'))
+const input = require('../very-basic-parsed.json')
 
-let currentChapterNumber = null
-let currentVerseNumber = null
-let currentNoteIdentifier = null
-let currentNoteNumbersToUniqueIdentifier = {}
-let noteIdentifiersToNotes = {}
-let versesAndNoteReferencesAndHeaders = []
-let lastIdentifier = 0
+const { versesAndNoteReferencesAndHeaders, noteIdentifiersToNotes } = processBasicDataStructure(input)
 
-function addToNote(text) {
-	if (!noteIdentifiersToNotes[currentNoteIdentifier]) {
-		noteIdentifiersToNotes[currentNoteIdentifier] = []
-	}
-
-	noteIdentifiersToNotes[currentNoteIdentifier].push(text)
-}
-
-let paragraphisOpen = false
-
-function startParagraphIfNoneIsOpen() {
-	if (!paragraphisOpen) {
-		paragraphisOpen = true
-		versesAndNoteReferencesAndHeaders.push({
-			type: 'start paragraph'
-		})
-	}
-}
-
-function closeParagraphIfAnyIsOpen() {
-	if (paragraphisOpen) {
-		paragraphisOpen = false
-		versesAndNoteReferencesAndHeaders.push({
-			type: 'end paragraph'
-		})
-	}
-}
-
-relevant.forEach(o => {
-	if (o.type === 'verse') {
-		startParagraphIfNoneIsOpen()
-		const parsedVerseText = parseVerse(currentVerseNumber, o.text)
-		currentVerseNumber = parsedVerseText.verseNumber
-		versesAndNoteReferencesAndHeaders = versesAndNoteReferencesAndHeaders.concat(parsedVerseText.verses.map(o => {
-			return {
-				type: 'verse',
-				verseNumber: o.verseNumber,
-				chapterNumber: currentChapterNumber,
-				text: o.text
-			}
-		}))
-	} else if (o.type === 'chapter number') {
-		closeParagraphIfAnyIsOpen()
-		currentChapterNumber = parseInt(o.text)
-		currentVerseNumber = 1
-	} else if (o.type === 'note number') {
-		const number = parseInt(o.text)
-		currentNoteIdentifier = currentNoteNumbersToUniqueIdentifier[number]
-	} else if (o.type === 'note reference') {
-		const number = parseInt(o.text)
-		const identifier = unique()
-		currentNoteNumbersToUniqueIdentifier[number] = identifier
-		versesAndNoteReferencesAndHeaders.push({
-			type: 'note reference',
-			identifier: identifier
-		})
-	} else if (o.type === 'verse') {
-		startParagraphIfNoneIsOpen()
-		versesAndNoteReferencesAndHeaders.push({
-			type: 'verse',
-			chapterNumber: currentChapterNumber,
-			verseNumber: currentVerseNumber,
-			text: o.text
-		})
-	} else if (o.type === 'note') {
-		addToNote(o.text)
-	} else if (o.type === 'header') {
-		closeParagraphIfAnyIsOpen()
-		versesAndNoteReferencesAndHeaders.push({
-			type: 'header',
-			text: o.text
-		})
-	} else if (o.type === 'paragraph break') {
-		closeParagraphIfAnyIsOpen()
-	}
-})
-
-closeParagraphIfAnyIsOpen()
+const appropriatelyChunkedVerses = flatMap(combineAdjacentVerseChunks(versesAndNoteReferencesAndHeaders), splitVerseIfNecessary)
+write('verses-note-references-and-headers', addVerseSectionNumbers(appropriatelyChunkedVerses))
+write('notes', combineNoteArrays(noteIdentifiersToNotes))
 
 function combineAdjacentVerseChunks(versesAndNoteReferencesAndHeaders) {
 	let last = null
@@ -140,12 +58,153 @@ function combineNoteArrays(noteIdentifiersToNotes) {
 	return transformed
 }
 
-function unique() {
-	lastIdentifier++
+function flatMap(ary, mapFn) {
+	const output = []
 
-	return lastIdentifier.toString()
+	ary.forEach(item => {
+		const mapResult = mapFn(item)
+		if (Array.isArray(mapResult)) {
+			mapResult.forEach(item => output.push(item))
+		} else {
+			output.push(mapResult)
+		}
+	})
+
+	return output
 }
 
-fs.writeFileSync('./verses-note-references-and-headers.json', JSON.stringify(combineAdjacentVerseChunks(versesAndNoteReferencesAndHeaders), null, '\t'))
-fs.writeFileSync('./notes.json', JSON.stringify(combineNoteArrays(noteIdentifiersToNotes), null, '\t'))
+function processBasicDataStructure(input) {
+	let currentChapterNumber = null
+	let currentVerseNumber = null
+	let currentNoteIdentifier = null
+	let currentNoteNumbersToUniqueIdentifier = {}
+	let noteIdentifiersToNotes = {}
+	let versesAndNoteReferencesAndHeaders = []
+	let lastIdentifier = 0
+	let paragraphisOpen = false
 
+	function unique() {
+		lastIdentifier++
+
+		return lastIdentifier.toString()
+	}
+
+	function addToNote(text) {
+		if (!noteIdentifiersToNotes[currentNoteIdentifier]) {
+			noteIdentifiersToNotes[currentNoteIdentifier] = []
+		}
+
+		noteIdentifiersToNotes[currentNoteIdentifier].push(text)
+	}
+
+	function startParagraphIfNoneIsOpen() {
+		if (!paragraphisOpen) {
+			paragraphisOpen = true
+			versesAndNoteReferencesAndHeaders.push({
+				type: 'start paragraph'
+			})
+		}
+	}
+
+	function closeParagraphIfAnyIsOpen() {
+		if (paragraphisOpen) {
+			paragraphisOpen = false
+			versesAndNoteReferencesAndHeaders.push({
+				type: 'end paragraph'
+			})
+		}
+	}
+
+
+	const relevant = input.filter(o => o.type !== 'UNKNOWN' && !(o.text && o.text.toLowerCase() === 'revelation'))
+
+	relevant.forEach(o => {
+		if (o.type === 'verse') {
+			startParagraphIfNoneIsOpen()
+			const parsedVerseText = parseVerse(currentVerseNumber, o.text)
+			currentVerseNumber = parsedVerseText.verseNumber
+			versesAndNoteReferencesAndHeaders = versesAndNoteReferencesAndHeaders.concat(parsedVerseText.verses.map(o => {
+				return {
+					type: 'verse',
+					verseNumber: o.verseNumber,
+					chapterNumber: currentChapterNumber,
+					text: o.text
+				}
+			}))
+		} else if (o.type === 'chapter number') {
+			closeParagraphIfAnyIsOpen()
+			currentChapterNumber = parseInt(o.text)
+			currentVerseNumber = 1
+		} else if (o.type === 'note number') {
+			const number = parseInt(o.text)
+			currentNoteIdentifier = currentNoteNumbersToUniqueIdentifier[number]
+		} else if (o.type === 'note reference') {
+			const number = parseInt(o.text)
+			const identifier = unique()
+			currentNoteNumbersToUniqueIdentifier[number] = identifier
+			versesAndNoteReferencesAndHeaders.push({
+				type: 'note reference',
+				identifier: identifier
+			})
+		} else if (o.type === 'verse') {
+			startParagraphIfNoneIsOpen()
+			versesAndNoteReferencesAndHeaders.push({
+				type: 'verse',
+				chapterNumber: currentChapterNumber,
+				verseNumber: currentVerseNumber,
+				text: o.text
+			})
+		} else if (o.type === 'note') {
+			addToNote(o.text)
+		} else if (o.type === 'header') {
+			closeParagraphIfAnyIsOpen()
+			versesAndNoteReferencesAndHeaders.push({
+				type: 'header',
+				text: o.text
+			})
+		} else if (o.type === 'paragraph break') {
+			closeParagraphIfAnyIsOpen()
+		}
+	})
+
+	closeParagraphIfAnyIsOpen()
+
+	return { versesAndNoteReferencesAndHeaders, noteIdentifiersToNotes }
+}
+
+function write(file, data) {
+	fs.writeFileSync(`./${file}.json`, JSON.stringify(data, null, '\t'))
+}
+
+function splitVerseIfNecessary(verse) {
+	if (verse.chapterNumber === 12 && verse.verseNumber === 17) {
+		return chunk(verse, [
+			'So the dragon was furious about the woman and off he went to make war with the rest of her offspring,',
+			'those who keep the commands of God and hold the testimony of Jesus.'
+		])
+	} else {
+		return verse
+	}
+}
+
+function chunk(verse, chunks) {
+	return chunks.map(text => Object.assign({}, verse, { text }))
+}
+
+function addVerseSectionNumbers(verses) {
+	let lastVerseNumber = 0
+	let sectionNumber = 0
+
+	return verses.map(chunk => {
+		if (chunk.type !== 'verse') {
+			return chunk
+		}
+
+		if (chunk.verseNumber !== lastVerseNumber) {
+			sectionNumber = 0
+		}
+		sectionNumber++
+		lastVerseNumber = chunk.verseNumber
+		return Object.assign({}, chunk, { sectionNumber })
+	})
+}
